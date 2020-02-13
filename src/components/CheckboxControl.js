fb18d76b2bcab875a19024d497bd2efb8d935e5c
z2lai <zhenglai3@gmail.com>
2020-02-12 21:30:48 -0500

    (WIP) Styling CheckboxControl using emotion's styled components

--- src/components/CheckboxControl.js
+++ src/components/CheckboxControl.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { jsx, css } from "@emotion/core";
+import styled from '@emotion/styled';
 /** @jsx jsx */
 
-const style = css`
-  color: hotpink;
+const StyledCheckbox = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 25%;
+  padding: 10px 5px;
+  background: ${props => props.checked ? '#fff200' : '#4b4b4b'};
 `;
 
 const Checkbox = props => (
-  <label>
+  <StyledCheckbox checked={props.checked}>
     <input type="checkbox" {...props} />
     {props.value};
-  </label>
+  </StyledCheckbox>
 );
 
 const CheckboxControl = props => {
